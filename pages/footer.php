4c8bb05eb7738e4e891a58750adb14b8bacf0eab
Juan Oliveira <juan.neuroengenharia@gmail.com>
2021-08-25 10:33:12 -0300

    v2

--- pages/footer.php
+++ pages/footer.php
@@ -23,6 +23,44 @@
             </div>
         </div>
     </div>
+
+    
 </body>
 
+<footer>
+<!-- Grid container -->
+<div class="container pt-4 text-center text-white">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+     <!-- Linkedin -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://www.linkedin.com/company/life-equipamentos-eletronicos/"
+        role="button"
+        target="_blank"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+      <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://github.com/juan-engmed/prova-final"
+        role="button"
+        target="_blank"
+        data-mdb-ripple-color="dark"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center text-dark p-3 shadow-sm text-muted" style="background-color: rgba(0, 0, 0, 0.2);">
+    © Copyright 1994-2021
+    <a class="text-dark" href="http://www.lifeonline.com.br/">Life Equipamentos</a>
+    <div>Desenvolvido por Juan Oliveira</div>
+  </div>
+
+  </footer>
 </html>
